40050099a3fbb52c0faae48aeb36ec5ed986c1ad
zantaclaus <tarzuto@gmail.com>
2022-09-13 18:39:56 +0700

    finished

--- rush/assets/js/index.js
+++ rush/assets/js/index.js
@@ -1,4 +1,26 @@
+let count = 0;
+
 function scrollPage(id) {
   const page = document.getElementById(id);
   page.scrollIntoView();
+}
+
+function randomColor(id) {
+  const randomColor = Math.floor(Math.random() * 0xffffff).toString(16);
+
+  const element = document.getElementById(id);
+  element.style.color = '#' + randomColor;
+}
+
+function addCount() {
+  count += 1;
+
+  if (count === 10) {
+    alertFlag();
+    count = 0;
+  }
+}
+
+function alertFlag() {
+  alert('I Love 42Bangkok');
 }
